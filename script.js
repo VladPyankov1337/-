91640c32d787c832ef2b3d53e0b6eeb31be2356c
VladPyankov1337 <165372106+VladPyankov1337@users.noreply.github.com>
2024-12-11 04:23:52 +0500

    52

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-const slider = document.querySelector(".mainphotos");
-const prevButton = document.querySelector(".prev__button");
-const nextButton = document.querySelector(".next__button");
-const slides = Array.from(slider.querySelectorAll("img"));
-const slideCount = slides.length;
-let slideIndex = 0;
-
-prevButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
-
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
-
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = "block";
-    } else {
-      slide.style.display = "none";
-    }
-  });
-}
-
-updateSlider();
-
 document.addEventListener("DOMContentLoaded", () => {
+  // Инициализация слайдера
+  const slider = document.querySelector(".mainphotos");
+  const prevButton = document.querySelector(".prev__button");
+  const nextButton = document.querySelector(".next__button");
+  const slides = Array.from(slider.querySelectorAll("img"));
+  const slideCount = slides.length;
+  let slideIndex = 0;
+
+  if (window.innerWidth <= 720) {
+    // Слайдер работает только на мобильных устройствах
+    prevButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+
+    function showPreviousSlide() {
+      slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+      updateSlider();
+    }
+
+    function showNextSlide() {
+      slideIndex = (slideIndex + 1) % slideCount;
+      updateSlider();
+    }
+
+    function updateSlider() {
+      slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+          slide.style.display = "block";
+        } else {
+          slide.style.display = "none";
+        }
+      });
+    }
+
+    updateSlider();
+  }
+
+  // Инициализация бургер-меню
   const burgerMenu = document.querySelector(".burger_menu");
   const logoText3 = document.querySelector(".logo-text3");
 
-  burgerMenu.addEventListener("click", () => {
-    logoText3.classList.toggle("active");
-  });
+  if (window.innerWidth <= 720) {
+    // Бургер-меню работает только на мобильных устройствах
+    burgerMenu.addEventListener("click", () => {
+      logoText3.classList.toggle("active");
+    });
+  }
 });
